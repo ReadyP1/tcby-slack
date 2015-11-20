13990526d3278f5f4cc560eb01b18ae5b4dc0b32
Julian A <julian.a@tuta.io>
2015-11-20 11:21:57 -0500

    New Relic?

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Slack Hubot'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '12d0dc6101ce51260313c7615a9fad07929f76bc',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
